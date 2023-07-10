9acc1306b5ab3a3701e61ac53f7664adbd0af55a
Ba Linh <linhnb@gmail.com>
2023-07-10 15:27:29 +0200

    Revert "test revert"
    This reverts commit 44504eb1e378b65c9ea02a60c0197a36bcb4a304.

--- test/src/ComputeCheckDigit.java
+++ test/src/ComputeCheckDigit.java
@@ -1,24 +1,24 @@
 public class ComputeCheckDigit {
-    public static void main(String args[]) {
+    public static void main(String args[]){
         int check = computeCheckDigit("39847");
         System.out.println(check);
     }
 
-    public static int computeCheckDigit(String identificationNumber) {
+    public static int computeCheckDigit(String identificationNumber){
         int sommePaire = 0;
         int sommeImpaire = 0;
         int result = 0;
-        for (int i = 0; i < identificationNumber.length(); i++) {
+        for(int i=0;i<identificationNumber.length();i++){
             Integer value = Integer.valueOf("" + identificationNumber.charAt(i));
-            if (i % 2 == 0) {
-                sommePaire += value;
-            } else {
-                sommeImpaire += value;
+            if(i%2==0){
+                sommePaire+= value;
+            }else{
+                sommeImpaire+= value;
             }
 
         }
-        result = sommePaire * 3 + sommeImpaire;
-        int dernierChiffre = result % 10;
-        return dernierChiffre == 0 ? 0 : 10 - dernierChiffre;
+        result = sommePaire*3+sommeImpaire;
+        int dernierChiffre = result%10;
+        return dernierChiffre==0?0:10-dernierChiffre;
     }
 }
