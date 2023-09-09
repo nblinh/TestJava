6428a188109d8915ac6e7f7825d2648a93b7f41d
Ba Linh <linhnb@gmail.com>
2023-09-09 12:51:22 +0200

    format file

--- test/src/Encode.java
+++ test/src/Encode.java
@@ -3,22 +3,22 @@ public class Encode {
         System.out.println(encode("aaaabcccaaa"));
     }
 
-    public static String encode(String plainText){
+    public static String encode(String plainText) {
         StringBuilder result = new StringBuilder("");
-        int count=0;
-        char ch=' ';
-        for(int i=0;i<plainText.length();i++){
-            if(i==0){
-                count=1;
-            }else{
-                if(plainText.charAt(i)==ch){
+        int count = 0;
+        char ch = ' ';
+        for (int i = 0; i < plainText.length(); i++) {
+            if (i == 0) {
+                count = 1;
+            } else {
+                if (plainText.charAt(i) == ch) {
                     count++;
-                }else{
+                } else {
                     result.append(count).append(ch);
-                    count=1;
+                    count = 1;
                 }
             }
-            ch=plainText.charAt(i);
+            ch = plainText.charAt(i);
         }
         result.append(count).append(ch);
         return result.toString();
